3f48a12a21748bed8f6b8b0c29586c0955c80927
suhaila <sosoissa04@gmail.com>
2025-02-02 18:59:18 +0200

    update images

--- main.py
+++ main.py
@@ -17,6 +17,21 @@ root.iconphoto(False,image_icon)
 
 
 
+color_box = PhotoImage(file="images/colorsection.png")
+Label(root, image=color_box, bg="#f2f3f5").place(x=10, y=20)
+
+eraser = PhotoImage(file="images/eraser.png")
+Button(root, image=eraser, bg="#f2f3f5").place(x=30, y=400)
+
+import_image = PhotoImage(file="images/addimage.png")
+Button(root, image=import_image, bg="white").place(x=30, y=450)
+
+
+canvas = Canvas(root, width=930, height=500, background="white", cursor="hand2")
+canvas.place(x=100, y=10)
+
+
+
 
 
 root.mainloop()
